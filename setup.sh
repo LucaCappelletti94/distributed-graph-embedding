82e5f40a5471ffef9477518a14d2087026df75ba
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-18 10:18:40 +0200

    Added commit

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ git clone https://github.com/SchedMD/slurm-gcp
 cd slurm-gcp
 wget https://raw.githubusercontent.com/LucaCappelletti94/distributed-graph-embedding/master/slurm.yaml
 gcloud deployment-manager deployments create distributed-ppi --config slurm.yaml
+# Remember to remove os-login= TRUE, or at least, I do not know how to connect to the login node otherwise
